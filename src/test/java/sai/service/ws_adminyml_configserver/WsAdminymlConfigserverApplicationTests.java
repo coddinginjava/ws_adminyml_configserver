39fdd9d52152d3bd05f7cb99ffe21a05c2d81be0
codding in java <coddinginjava@gmail.com>
2019-09-04 09:52:14 +0000

    Initial commit

---
+++ src/test/java/sai/service/ws_adminyml_configserver/WsAdminymlConfigserverApplicationTests.java
@@ -0,0 +1,16 @@
+package sai.service.ws_adminyml_configserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WsAdminymlConfigserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
